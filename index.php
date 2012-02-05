5872a009ea1a06b18180b992f045404f1a87dfd1
Nicolas Seriot <nicolas@seriot.ch>
2012-02-05 13:11:40 +0100

    added my personal google analytics tracking code

--- index.php
+++ index.php
@@ -123,5 +123,19 @@ if($display_mode == 'titles') {
 
      <div id="footer"><?=$footer_text?></div>
 
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-987064-3']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+
     </body>
 </html>
